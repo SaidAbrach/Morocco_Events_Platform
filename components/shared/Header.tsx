a06f8e29038ed3b1c52fb8f04e08db53c2ce2e74
Said Abrach <saidabrach02@gmail.com>
2024-11-07 21:17:53 +0100

    step by step

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -13,22 +13,22 @@ const Header = () => {
           <Image src="/assets/images/logo.svg" width={150} height={38} alt="Logo" />
         </Link>
         <SignedIn>
-            <nav className="md:flex-between hidden w-full max-w-xs">
-                <NavItems />
-            </nav>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
         </SignedIn>
         <div className="flex w-32 justify-end gap-3">
-            <SignedIn>
-                <UserButton afterSignOutUrl="/" />
-                <MobileNav />
-            </SignedIn>
-            <SignedOut>
-                <Button asChild className="rounded-full" size="lg">
-                    <Link href="/sign-in">
-                    Login
-                    </Link>
-                </Button>
-            </SignedOut>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">
+                Login
+              </Link>
+            </Button>
+          </SignedOut>
         </div>
       </div>
     </header>
---
+++ lib/actions/category.actions.ts
@@ -0,0 +1,31 @@
+"use server"
+
+import { CreateCategoryParams } from "@/types"
+import { connectToDatabse } from "../database"
+import { handleError } from "../utils";
+import Category from "../database/models/category.model";
+
+export const createCategory = async ({ categoryName } :CreateCategoryParams) => {
+    try {
+        await connectToDatabse();
+
+        const newCategory = await Category.create({ name: categoryName });
+
+        return JSON.parse(JSON.stringify(newCategory));
+    }catch (error){
+        handleError(error)
+
+    }
+}
+export const getAllCategories = async () => {
+    try {
+        await connectToDatabse();
+
+        const categories = await Category.find();
+
+        return JSON.parse(JSON.stringify(categories));
+    }catch (error){
+        handleError(error)
+
+    }
+}
